609c2150c9db11d251c01156bb384e300fda9c6e
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2020-12-05 15:53:11 -0600

    Solve advent day 4

---
+++ tests/test_day3.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestDay3Solution(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_day_03_part_one(self):
+        pass
+
+    def test_day_03_part_two(self):
+        pass
