2b6e1b2f81af5a52b1ede86ffabe4012511344aa
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2020-12-05 15:52:27 -0600

    Rename tests to common structure

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -16,28 +16,13 @@ class TestDay1Solution(unittest.TestCase):
         self.test_answer_part_one = 514579
         self.test_answer_part_two = 241861950
 
-    def test_find_2_complement_values(self):
-        """
-        Correctly find the two entries that sum to 2020 in the test data
-        """
+    def test_day_01_part_one(self):
         x, y = find_n_complement_values(self.test_input, 2020, 2)
         self.assertEqual(x * y, self.test_answer_part_one)
 
-    def test_find_3_complement_values(self):
-        """
-        Correctly find the three entries that sum to 2020 in the test data
-        """
+    def test_day_01_part_two(self):
         x, y, z = find_n_complement_values(self.test_input, 2020, 3)
         self.assertEqual(x * y * z, self.test_answer_part_two)
-
-    def test_no_target_found_zero(self):
-        """
-        If the target value cannot be found in the input list, then
-        find_n_complement_values should return a tuple of zeroes equal to the
-        argument passed to n_terms
-        """
-        self.assertEqual(find_n_complement_values(self.test_input, 0, 2), (0, 0))
-        self.assertEqual(find_n_complement_values(self.test_input, 0, 3), (0, 0, 0))
 
 
 if __name__ == "__main__":
